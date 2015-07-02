164a3ef663b0ec3851aca4bfbb639f4ecc5e843a
ferame <alisauskas.j@gmail.com>
2015-07-03 00:27:58 +0100

    y

---
+++ app/src/main/java/com/startup/edy/hellomoon/AudioPlayer.java
@@ -0,0 +1,32 @@
+package com.startup.edy.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by Verden1 on 27/06/2015.
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c) {
+        stop();
+
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+        mPlayer.start();
+    }
+}
---
+++ app/src/main/java/com/startup/edy/hellomoon/HelloMoonActivity.java
@@ -0,0 +1,15 @@
+package com.startup.edy.hellomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class HelloMoonActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+
+}
